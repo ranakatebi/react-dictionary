b5631f55a80470af8148e9711a8577219c31dda1
ranakatebi <rana_katebi@hotmail.com>
2022-03-17 16:00:13 +0300

    app js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary defaultKeyword="react" />
+          <Dictionary />
         </main>
         <footer className="App-footer">
           <small>
