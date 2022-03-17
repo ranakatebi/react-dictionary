ba44029e94a36f2bdefac9ece84e628ebd61ca4f
ranakatebi <rana_katebi@hotmail.com>
2022-03-17 13:43:45 +0300

    app js changed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary defaultKeyword="react" />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a href="https://github.com/ranakatebi/react-dictionary">
+              Coded by Rana Katebi
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
