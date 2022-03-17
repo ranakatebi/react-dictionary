4a8d8d809e973311b7ffc3fd819ac2274374377d
ranakatebi <rana_katebi@hotmail.com>
2022-03-17 15:37:26 +0300

    meaning changed

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,18 @@
 import React from "react";
-import "./Meaning.css";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <div className="definition">{definition.definition}</div>
-
-            <div className="example">{definition.example}</div>
-            <Synonyms synonyms={definition.synonyms} />
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
           </div>
         );
       })}
