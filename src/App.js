c7c0c09cfcb9f81f7bec37b8d271a3b6475c1376
ranakatebi <rana_katebi@hotmail.com>
2022-03-22 13:25:04 +0300

    defaultKeyword set

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <div className="container">
         <header className="App-header"></header>
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="React" />
         </main>
         <footer className="App-footer">
           <small>
